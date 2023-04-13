ededad44a6bd1f5a17ae9eea15f0ffa19b7c1bd2
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2023-04-12 22:33:04 -0300

    create date and price formatters

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,14 +1,16 @@
 import { useContext } from 'react'
 
-import { Header } from '../../components/Header'
-import { Summary } from '../../components/Summary'
-import { SearchForm } from './components/SearchForm'
 import {
   PriceHighlight,
   TransactionsContainer,
   TransactionsTable,
 } from './styles'
+
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext)
@@ -29,11 +31,13 @@ export function Transactions() {
                   <td>
                     <PriceHighlight variant={transaction.type}>
                       {transaction.type === 'outcome' && '- '}
-                      {transaction.price}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
               )
             })}
